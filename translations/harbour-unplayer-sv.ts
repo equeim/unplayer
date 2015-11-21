11064d165f8354a4398823e34d34ebd2416e027c
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-11-21 11:50:39 +0100

    Create harbour-unplayer-sv.ts
    Swedish translation

---
+++ translations/harbour-unplayer-sv.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AddToPlaylistPage</name>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>AlbumPageHeader</name>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>AllArtistsPage</name>
+    <message numerus="yes">
+        <source>%n album(s)</source>
+        <translation>
+            <numerusform>%n album</numerusform>
+            <numerusform>%n album</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ArtistPage</name>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>ArtistPageHeader</name>
+    <message numerus="yes">
+        <source>%n album(s)</source>
+        <translation>
+            <numerusform>%n album</numerusform>
+            <numerusform>%n album</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message numerus="yes">
+        <source>%n artist(s)</source>
+        <translation>
+            <numerusform>%n artist</numerusform>
+            <numerusform>%n artister</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n albums(s)</source>
+        <translation>
+            <numerusform>%n album</numerusform>
+            <numerusform>%n album</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n tracks(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n playlist(s)</source>
+        <translation>
+            <numerusform>%n spellista</numerusform>
+            <numerusform>%n spellistor</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistsPage</name>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation>
+            <numerusform>%n spår</numerusform>
+            <numerusform>%n spår</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
