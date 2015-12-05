8f9a6ae3c2496dd21777ffae7e6e7c74631dceb4
jgibbon <jngibbon@gmail.com>
2015-12-05 10:26:27 +0100

    Translated using Weblate (German)
    Currently translated at 100.0% (96 of 96 strings)

--- translations/harbour-unplayer-de.ts
+++ translations/harbour-unplayer-de.ts
@@ -5,462 +5,473 @@
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Über</translation>
     </message>
     <message>
         <source>Simple music player for Sailfish OS</source>
-        <translation type="unfinished"></translation>
+        <translation>Einfacher Musikabspieler für Sailfish OS</translation>
     </message>
     <message>
         <source>Show license</source>
-        <translation type="unfinished"></translation>
+        <translation>Lizenz anzeigen</translation>
     </message>
     <message>
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersetzungen</translation>
     </message>
     <message>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersetzer</translation>
     </message>
 </context>
 <context>
     <name>AddToPlaylistPage</name>
     <message numerus="yes">
         <source>%n track(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>New playlist...</source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Playlist…</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Playlists</translation>
     </message>
 </context>
 <context>
     <name>AlbumDelegate</name>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Set cover image</source>
-        <translation type="unfinished"></translation>
+        <translation>Coverbild setzen</translation>
     </message>
 </context>
 <context>
     <name>AlbumPage</name>
     <message>
         <source>No tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Lieder</translation>
     </message>
     <message>
         <source>Set cover image</source>
-        <translation type="unfinished"></translation>
+        <translation>Coverbild setzen</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>AlbumPageHeader</name>
     <message numerus="yes">
         <source>%n track(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
 </context>
 <context>
     <name>AlbumsModel</name>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekanntes Album</translation>
     </message>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Interpret</translation>
     </message>
 </context>
 <context>
     <name>AllAlbumsPage</name>
     <message>
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Alben</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Alben</translation>
     </message>
 </context>
 <context>
     <name>AllArtistsPage</name>
     <message numerus="yes">
         <source>%n album(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Album</numerusform>
+        <numerusform>%n Alben</numerusform>
         </translation></message>
     <message>
         <source>Artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Interpreten</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Interpreten</translation>
     </message>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>AllTracksPage</name>
     <message>
         <source>No tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Lieder</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>ArtistPage</name>
     <message numerus="yes">
         <source>%n track(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
     <message>
         <source>All tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle Lieder</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Alben</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>ArtistPageHeader</name>
     <message numerus="yes">
         <source>%n album(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Album</numerusform>
+        <numerusform>%n Alben</numerusform>
         </translation></message>
     <message numerus="yes">
         <source>%n track(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
 </context>
 <context>
     <name>ArtistsModel</name>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Interpret</translation>
     </message>
 </context>
 <context>
     <name>LibraryTrackDelegate</name>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>LibraryTracksPullDownMenu</name>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
 </context>
 <context>
     <name>LicensePage</name>
     <message>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Lizenz</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message numerus="yes">
         <source>%n artist(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Interpret</numerusform>
+        <numerusform>%n Interpreten</numerusform>
         </translation></message>
     <message numerus="yes">
         <source>%n albums(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Album</numerusform>
+        <numerusform>%n Alben</numerusform>
         </translation></message>
     <message numerus="yes">
         <source>%n tracks(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
     <message numerus="yes">
         <source>%n playlist(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Playlist</numerusform>
+        <numerusform>%n Playlisten</numerusform>
         </translation></message>
     <message>
         <source>Artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Interpreten</translation>
     </message>
     <message>
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Alben</translation>
     </message>
     <message>
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Lieder</translation>
     </message>
     <message>
         <source>Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlisten</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Über</translation>
     </message>
 </context>
 <context>
     <name>NewPlaylistDialog</name>
     <message>
         <source>Add playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlist hinzufügen</translation>
     </message>
     <message>
         <source>Playlist name</source>
-        <translation type="unfinished"></translation>
+        <translation>Name der Playlist</translation>
     </message>
 </context>
 <context>
     <name>NowPlayingPage</name>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>PlaylistPage</name>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Warteschlange hinzufügen</translation>
     </message>
     <message>
         <source>Remove from playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Aus der Playlist entfernen</translation>
     </message>
     <message>
         <source>Removing</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne</translation>
     </message>
     <message>
         <source>Remove playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne Playlist</translation>
     </message>
     <message>
         <source>Removing playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne Playlist</translation>
     </message>
     <message>
         <source>Clear playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlist leeren</translation>
     </message>
     <message>
         <source>Clearing playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Leere Playlist</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>Loading</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade</translation>
     </message>
     <message>
         <source>No tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Lieder</translation>
     </message>
 </context>
 <context>
     <name>PlaylistsPage</name>
     <message numerus="yes">
         <source>%n track(s)</source>
-        <translation type="unfinished"><numerusform></numerusform>
+        <translation><numerusform>%n Lied</numerusform>
+        <numerusform>%n Lieder</numerusform>
         </translation></message>
     <message>
         <source>Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlisten</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne</translation>
     </message>
     <message>
         <source>Removing</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne</translation>
     </message>
     <message>
         <source>New playlist...</source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Playlist…</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Playlists</translation>
     </message>
 </context>
 <context>
     <name>QueuePage</name>
     <message>
         <source>Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Warteschlange</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne</translation>
     </message>
     <message>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Leeren</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
     <message>
         <source>No tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Lieder</translation>
     </message>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Playlist hinzufügen</translation>
     </message>
 </context>
 <context>
     <name>SearchPullDownMenuItem</name>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Suche</translation>
     </message>
 </context>
 <context>
     <name>SetCoverPage</name>
     <message>
         <source>Select JPEG file</source>
-        <translation type="unfinished"></translation>
+        <translation>JPEG-Datei auswählen</translation>
     </message>
     <message>
         <source>SD card</source>
-        <translation type="unfinished"></translation>
+        <translation>SD-Karte</translation>
     </message>
     <message>
         <source>Home directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Home-Verzeichnis</translation>
     </message>
     <message>
         <source>No files</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine Dateien</translation>
     </message>
 </context>
 <context>
     <name>TracksModel</name>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Interpret</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekanntes Album</translation>
     </message>
 </context>
 <context>
     <name>Unplayer::PlaylistModel</name>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Interpret</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekanntes Album</translation>
     </message>
 </context>
 <context>
     <name>Unplayer::Utils</name>
     <message>
         <source>%1 h </source>
-        <translation type="unfinished"></translation>
+        <translation>%1 h </translation>
     </message>
     <message>
         <source>%1 m </source>
-        <translation type="unfinished"></translation>
+        <translation>%1 m </translation>
     </message>
     <message>
         <source>%1 s</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 s</translation>
     </message>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Interpret</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekanntes Album</translation>
     </message>
 </context>
 </TS>
