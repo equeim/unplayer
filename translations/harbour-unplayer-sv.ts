9b95600bb35a8d2c4d69bf358fe15c9cb144677e
Åke Engelbrektson <eson@svenskasprakfiler.se>
2015-12-13 08:10:06 +0100

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (170 of 170 strings)

--- translations/harbour-unplayer-sv.ts
+++ translations/harbour-unplayer-sv.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv">
 <context>
@@ -28,7 +28,7 @@
     <name>AddToPlaylistMenuItem</name>
     <message>
         <source>Add to playlist</source>
-        <translation type="unfinished">Lägg till i spellista</translation>
+        <translation>Lägg till i spellista</translation>
     </message>
 </context>
 <context>
@@ -61,7 +61,7 @@
     <name>AddToQueueMenuItem</name>
     <message>
         <source>Add to queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Lägg till i kön</translation>
     </message>
 </context>
 <context>
@@ -99,14 +99,12 @@
     </message>
     <message numerus="yes">
         <source>%n track(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår markerat</numerusform>
+        <numerusform>%n spår markerade</numerusform>
+        </translation></message>
     <message>
         <source>Select tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spår</translation>
     </message>
 </context>
 <context>
@@ -146,14 +144,12 @@
     </message>
     <message numerus="yes">
         <source>%n album(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n album markerat</numerusform>
+        <numerusform>%n album markerade</numerusform>
+        </translation></message>
     <message>
         <source>Select albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj album</translation>
     </message>
 </context>
 <context>
@@ -232,14 +228,12 @@
     </message>
     <message numerus="yes">
         <source>%n album(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n album markerat</numerusform>
+        <numerusform>%n album markerade</numerusform>
+        </translation></message>
     <message>
         <source>Select albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj album</translation>
     </message>
 </context>
 <context>
@@ -270,100 +264,92 @@
     <name>ArtistsPage</name>
     <message numerus="yes">
         <source>%n artist(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n artist markerad</numerusform>
+        <numerusform>%n artister markerade</numerusform>
+        </translation></message>
     <message>
         <source>Artists</source>
-        <translation type="unfinished">Artister</translation>
+        <translation>Artister</translation>
     </message>
     <message numerus="yes">
         <source>%n album(s)</source>
-        <translation type="unfinished">
+        <translation>
             <numerusform>%n album</numerusform>
             <numerusform>%n album</numerusform>
         </translation>
     </message>
     <message>
         <source>All tracks</source>
-        <translation type="unfinished">Alla spår</translation>
+        <translation>Alla spår</translation>
     </message>
     <message>
         <source>Select artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj artister</translation>
     </message>
     <message>
         <source>No artists</source>
-        <translation type="unfinished">Inga artister</translation>
+        <translation>Inga artister</translation>
     </message>
 </context>
 <context>
     <name>DirectoriesPage</name>
     <message numerus="yes">
         <source>%n track(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår markerat</numerusform>
+        <numerusform>%n spår markerade</numerusform>
+        </translation></message>
     <message>
         <source>Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Mappar</translation>
     </message>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
     <message>
         <source>SD card</source>
-        <translation type="unfinished">SD-kort</translation>
+        <translation>SD-kort</translation>
     </message>
     <message>
         <source>Home directory</source>
-        <translation type="unfinished">Hemkatalog</translation>
+        <translation>Hemkatalog</translation>
     </message>
     <message>
         <source>Select tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spår</translation>
     </message>
     <message>
         <source>Loading</source>
-        <translation type="unfinished">Läser in</translation>
+        <translation>Läser in</translation>
     </message>
     <message>
         <source>No files</source>
-        <translation type="unfinished">Inga filer</translation>
+        <translation>Inga filer</translation>
     </message>
 </context>
 <context>
     <name>GenresPage</name>
     <message numerus="yes">
         <source>%n genre(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n genre markerad</numerusform>
+        <numerusform>%n genrer markerade</numerusform>
+        </translation></message>
     <message>
         <source>Genres</source>
-        <translation type="unfinished"></translation>
+        <translation>Genrer</translation>
     </message>
     <message numerus="yes">
         <source>%n track(s), %1</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår, %1</numerusform>
+        <numerusform>%n spår, %1</numerusform>
+        </translation></message>
     <message>
         <source>Select genres</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj genrer</translation>
     </message>
     <message>
         <source>No genres</source>
-        <translation type="unfinished"></translation>
+        <translation>Inga genrer</translation>
     </message>
 </context>
 <context>
@@ -378,7 +364,7 @@
     </message>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
 </context>
 <context>
@@ -455,11 +441,11 @@
     </message>
     <message>
         <source>Genres</source>
-        <translation type="unfinished"></translation>
+        <translation>Genrer</translation>
     </message>
     <message>
         <source>Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Mappar</translation>
     </message>
 </context>
 <context>
@@ -481,7 +467,7 @@
     </message>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
 </context>
 <context>
@@ -528,25 +514,21 @@
     </message>
     <message numerus="yes">
         <source>%n track(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår markerat</numerusform>
+        <numerusform>%n spår markerade</numerusform>
+        </translation></message>
     <message numerus="yes">
         <source>Removing %n track(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Tar bort %n spår</numerusform>
+        <numerusform>Tar bort %n spår</numerusform>
+        </translation></message>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
     <message>
         <source>Select tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spår</translation>
     </message>
 </context>
 <context>
@@ -584,21 +566,17 @@
     </message>
     <message numerus="yes">
         <source>%n playlist(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spellista markerad</numerusform>
+        <numerusform>%n spellistor markerade</numerusform>
+        </translation></message>
     <message numerus="yes">
         <source>Removing %n playlist(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Tar bort %n spellista</numerusform>
+        <numerusform>Tar bort %n spellistor</numerusform>
+        </translation></message>
     <message>
         <source>Select playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spellistor</translation>
     </message>
 </context>
 <context>
@@ -629,25 +607,23 @@
     </message>
     <message numerus="yes">
         <source>%n track(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår markerat</numerusform>
+        <numerusform>%n spår markerade</numerusform>
+        </translation></message>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
     <message>
         <source>Select tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spår</translation>
     </message>
 </context>
 <context>
     <name>SearchMenuItem</name>
     <message>
         <source>Search</source>
-        <translation type="unfinished">Sök</translation>
+        <translation>Sök</translation>
     </message>
 </context>
 <context>
@@ -661,11 +637,11 @@
     <name>SelectionPanel</name>
     <message>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Alla</translation>
     </message>
     <message>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen</translation>
     </message>
 </context>
 <context>
@@ -688,62 +664,62 @@
     </message>
     <message>
         <source>Select image</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj bild</translation>
     </message>
 </context>
 <context>
     <name>TrackInfoPage</name>
     <message>
         <source>Track information</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårinformation</translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel</translation>
     </message>
     <message>
         <source>Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Artist</translation>
     </message>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished">Okänd artist</translation>
+        <translation>Okänd artist</translation>
     </message>
     <message>
         <source>Album</source>
-        <translation type="unfinished"></translation>
+        <translation>Album</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished">Okänt album</translation>
+        <translation>Okänt album</translation>
     </message>
     <message>
         <source>Track number</source>
-        <translation type="unfinished"></translation>
+        <translation>Spårnummer</translation>
     </message>
     <message>
         <source>Genre</source>
-        <translation type="unfinished"></translation>
+        <translation>Genre</translation>
     </message>
     <message>
         <source>File path</source>
-        <translation type="unfinished"></translation>
+        <translation>Filsökväg</translation>
     </message>
     <message>
         <source>File size</source>
-        <translation type="unfinished"></translation>
+        <translation>Filstorlek</translation>
     </message>
     <message>
         <source>MIME type</source>
-        <translation type="unfinished"></translation>
+        <translation>MIME-typ</translation>
     </message>
     <message>
         <source>Duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Varaktighet</translation>
     </message>
     <message>
         <source>Bitrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Bithastighet</translation>
     </message>
 </context>
 <context>
@@ -761,29 +737,27 @@
     <name>TracksPage</name>
     <message numerus="yes">
         <source>%n track(s) selected</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n spår markerat</numerusform>
+        <numerusform>%n spår markerade</numerusform>
+        </translation></message>
     <message>
         <source>Select tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Välj spår</translation>
     </message>
     <message>
         <source>No tracks</source>
-        <translation type="unfinished">Inga spår</translation>
+        <translation>Inga spår</translation>
     </message>
 </context>
 <context>
     <name>Unplayer::DirectoryTracksModel</name>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished">Okänd artist</translation>
+        <translation>Okänd artist</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished">Okänt album</translation>
+        <translation>Okänt album</translation>
     </message>
 </context>
 <context>
@@ -801,11 +775,11 @@
     <name>Unplayer::PlaylistUtils</name>
     <message>
         <source>Unknown artist</source>
-        <translation type="unfinished">Okänd artist</translation>
+        <translation>Okänd artist</translation>
     </message>
     <message>
         <source>Unknown album</source>
-        <translation type="unfinished">Okänt album</translation>
+        <translation>Okänt album</translation>
     </message>
 </context>
 <context>
@@ -832,39 +806,39 @@
     </message>
     <message>
         <source>%1 B</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 B</translation>
     </message>
     <message>
         <source>%1 KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 KiB</translation>
     </message>
     <message>
         <source>%1 MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 MiB</translation>
     </message>
     <message>
         <source>%1 GiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 GiB</translation>
     </message>
     <message>
         <source>%1 TiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 TiB</translation>
     </message>
     <message>
         <source>%1 PiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 PiB</translation>
     </message>
     <message>
         <source>%1 EiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 EiB</translation>
     </message>
     <message>
         <source>%1 ZiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Zibyte</translation>
     </message>
     <message>
         <source>%1 YiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 YiByte</translation>
     </message>
 </context>
 </TS>
