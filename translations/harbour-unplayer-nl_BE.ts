05706fce9c0f3979453909c5bce2f888c84760c1
Nathan Follens <nathan@email.is>
2018-04-09 19:53:14 +0000

    Added translation using Weblate (Flemish)

---
+++ translations/harbour-unplayer-nl_BE.ts
@@ -0,0 +1,515 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_BE">
+<context>
+    <name>unplayer</name>
+    <message>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Simple music player for Sailfish OS</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Add to playlist</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n track(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>New playlist...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No playlists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Add to queue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set cover image</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No tracks</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n track(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Select tracks</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Unknown album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Unknown artist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Albums</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No albums</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n album(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Select albums</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n album(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Artists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No artists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>All tracks</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n artist(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Select artists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Track information</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>SD card</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Home directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No files</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n genre(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Genres</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n track(s), %1</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Select genres</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No genres</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n artist(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message numerus="yes">
+        <source>%n albums(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message numerus="yes">
+        <source>%n tracks(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message numerus="yes">
+        <source>%n playlist(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Tracks</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Playlists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Add playlist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Playlist name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Remove from playlist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Removing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Remove playlist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Removing playlist</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>Removing %n track(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Remove</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n playlist(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message numerus="yes">
+        <source>Removing %n playlist(s)</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Select playlists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Queue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Track number</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Genre</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>File path</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>File size</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Duration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Bitrate</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 B</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 KiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 MiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 GiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 TiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 PiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 EiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 ZiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 YiB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Source Code</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select Image</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Maintainer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 - %2</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n file(s) selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Set as default directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Default directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n directories selected</source>
+        <translation><numerusform></numerusform>
+        <numerusform></numerusform>
+        </translation></message>
+    <message>
+        <source>Library Directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Add directory...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Reset Library</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Update Library</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Library</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1, %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Updating library...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Error initializing database</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No selected directories</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Updating...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Clear Queue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Open library on startup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 kB/s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 h %2 m</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 m %2 s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Prefer cover art located as separate file in music file directory instead of extracted from music file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Restore player state on startup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Sort</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Order</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Ascending</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Album title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Album year</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Artist - Album title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Artist - Album year</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Added date</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Inside Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Sort Descending</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Sort Ascending</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
