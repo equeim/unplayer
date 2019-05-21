c37008286d19accb2abfcee426af0fd5c9c5d067
carlosgonz <45316884+GNUuser@users.noreply.github.com>
2019-05-20 21:54:53 -0400

    Update harbour-unplayer-es.ts

--- translations/harbour-unplayer-es.ts
+++ translations/harbour-unplayer-es.ts
@@ -741,11 +741,11 @@
     </message>
     <message>
         <source>Stop after playing track: &lt;font color=&quot;%1&quot;&gt;yes&lt;/font&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Parar después de reproducir la pista: &lt;font color=&quot;%1&quot;&gt;no&lt;/font&gt;</translation>
     </message>
     <message>
         <source>Stop after playing track: &lt;font color=&quot;%1&quot;&gt;no&lt;/font&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Parar después de reproducir la pista: &lt;font color=&quot;%1&quot;&gt;no&lt;/font&gt;</translation>
     </message>
 </context>
 </TS>
