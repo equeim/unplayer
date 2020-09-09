564ced13cadec604942046eb353f1ed26dd61c66
Allan Nordhøy <epost@anotheragency.no>
2020-09-09 23:12:30 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 95.1% (158 of 166 strings)

--- translations/harbour-unplayer-nb.ts
+++ translations/harbour-unplayer-nb.ts
@@ -749,7 +749,7 @@
     </message>
     <message>
         <source>Replace queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Erstatt kø</translation>
     </message>
 </context>
 </TS>
